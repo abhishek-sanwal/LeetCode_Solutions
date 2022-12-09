55bc021eb12153ddd232bc54c40815b3b5457144
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2022-12-09 21:42:37 +0530

    Time: 98 ms (91.37%), Space: 14.2 MB (67.92%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,12 +1,63 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         
+        def kthsmallest(nums1,nums2):
+            
+            k = floor((len(nums1) + len(nums2))/2) + 1
+            #print(k)
+            ptr1 = ptr2 = prev = curr = 0
+            
+            while k and ptr1 < len(nums1) and ptr2< len(nums2) :
+                
+                prev = curr
+                
+                if nums1[ptr1] < nums2[ptr2]:
+                    
+                    curr = nums1[ptr1]
+                    ptr1 += 1
+                    
+                else:
+                    
+                    curr = nums2[ptr2]
+                    ptr2 += 1
+                    
+                k -= 1
+            #print(curr,prev)
+            while k and ptr1 < len(nums1):
+                prev = curr
+                curr = nums1[ptr1]
+                ptr1+=1
+                k -= 1
+            #print(curr,prev)
+            while k and ptr2 < len(nums2):
+                prev = curr
+                curr = nums2[ptr2]
+                ptr2 += 1
+                k -= 1
+                
+            #print(curr,prev)
+            if (len(nums1) + len(nums2)) % 2 == 1:
+                
+                return float(curr)
+            
+            return (prev + curr)/ 2
+            
+            
+        
+        return kthsmallest(nums1,nums2)
+        
+        
+        
+        
+        
+        
+        
         arr = nums1 + nums2
         arr.sort()
         
         if len(arr)%2==0:
             
-            print(arr[len(arr)//2],arr[(len(arr)//2) -1])
+            #print(arr[len(arr)//2],arr[(len(arr)//2) -1])
             return (arr[len(arr)//2] + arr[(len(arr)//2) -1] )/2
         
         return float(arr[floor(len(arr)/2)])
