7ce3e5368bbe50f20b84e564fb37208acecd8996
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2022-12-13 22:39:54 +0530

    Time: 33 ms (99.44%), Space: 14.1 MB (43.93%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -1,6 +1,30 @@
 class Solution:
     def splitArray(self, arr: List[int], K: int) -> int:
         
+        def valid(mid,arr,K):
+            
+            subarr=0
+    
+            su=0
+    
+            for i in range(len(arr)):
+    
+                su+=arr[i]
+
+                if (su>mid):
+
+                    subarr+=1
+
+                    su=arr[i]
+
+            if (subarr+1)<=K:
+                
+                return True
+            
+            return False
+        
+        
+        
         N = len(arr)
         mi=max(arr)
     
@@ -12,24 +36,8 @@ class Solution:
     
             mid=(mi+ma)//2
     
-            subarr=0
-    
-            su=0
-    
-            for i in range(N):
-    
-                su+=arr[i]
-
-                if (su>mid):
-
-                    subarr+=1
-
-                    su=arr[i]
-
-            if (subarr+1)<=K:
-
+            if valid(mid,arr,K):
                 ans=mid
-
                 ma=mid-1
 
             else:
