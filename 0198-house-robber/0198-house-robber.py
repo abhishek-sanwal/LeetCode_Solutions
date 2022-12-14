44f783e096b7569624427b9a5ad7e6db1a37941f
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2022-12-14 11:53:12 +0530

    Time: 35 ms (88.52%), Space: 13.8 MB (97.62%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -2,7 +2,17 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         arr = nums
         if len(nums) == 1:
-            return nums[-0]
+            return nums[0]
+        
+        prev = nums[0]
+        curr = max(nums[0],nums[1])
+        
+        for i in range(2,len(nums)):
+            
+            prev,curr = curr,max(curr,nums[i] + prev)
+            
+        return curr
+        
         
         dp = [0]*len(nums)
         
@@ -13,7 +23,7 @@ class Solution:
             
             dp[i] = max(dp[i-1],arr[i]+dp[i-2])
             
-        
+        print(dp)
         return dp[-1]
             
             
