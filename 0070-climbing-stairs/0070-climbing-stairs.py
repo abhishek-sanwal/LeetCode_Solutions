831c396c0f7602555b3e20625b81c60a6a2ce8b0
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2022-12-19 17:31:18 +0530

    Time: 59 ms (28.36%), Space: 13.8 MB (57.82%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -41,8 +41,8 @@ class Solution:
                 dp[i] = dp[i-1] + dp[i-2]
             
             return dp[n]
-        TopDown(n)
-        # BottomUp(n)
+        #TopDown(n)
+        BottomUp(n)
         return dp[n]
         
         x,y = 1,1
