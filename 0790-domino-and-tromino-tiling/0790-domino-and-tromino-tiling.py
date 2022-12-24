d49f72a930c103794f90c96cd76ebd9af297291f
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2022-12-24 18:15:40 +0530

    Time: 38 ms (89.37%), Space: 13.8 MB (85.40%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -3,6 +3,8 @@ class Solution:
         
         mod = (10**9) + 7
         
+        # for n==0 we need to return 1 but it is not valid tc so avoid.
+        
         if n <3:
             return n
         
@@ -12,7 +14,7 @@ class Solution:
         
         for i in range(4,n+1):
             
-            dp[-3],dp[-2],dp[-1] = dp[-2],dp[-1],(2*(dp[-1]))%mod + (dp[-3])%mod
+            dp[-3],dp[-2],dp[-1] = dp[-2],dp[-1],((2*(dp[-1]))%mod + dp[-3])%mod
             
             
-        return dp[-1]%mod
+        return dp[-1]
