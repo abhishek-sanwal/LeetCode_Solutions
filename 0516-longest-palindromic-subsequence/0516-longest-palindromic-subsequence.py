bf1a14a1f480a3bcc034d3b3ad8c748de89bdf4b
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2023-04-24 22:49:24 +0530

    Time: 3373 ms (8.38%), Space: 39.1 MB (46.33%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -0,0 +1,27 @@
+class Solution:
+    def LCS(self,X , Y):
+    
+        m = len(X)
+        n = len(Y)
+     
+        
+        L = [[None]*(n+1) for i in range(m+1)]
+     
+        for i in range(m+1):
+            for j in range(n+1):
+                if i == 0 or j == 0 :
+                    L[i][j] = 0
+                elif X[i-1] == Y[j-1]:
+                    L[i][j] = L[i-1][j-1]+1
+                else:
+                    L[i][j] = max(L[i-1][j] , L[i][j-1])
+     
+        return L[m][n]    
+        
+
+    
+    def longestPalindromeSubseq(self, s: str) -> int:
+        
+        return self.LCS(s,s[::-1])
+        
+        
