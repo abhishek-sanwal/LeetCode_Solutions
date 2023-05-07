d8f5464b6d874ba3612bb9ef986d8a5fa55e51f1
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2023-05-08 00:04:27 +0530

    Time: 4 ms (31.84%), Space: 5.9 MB (66.84%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
@@ -1,61 +1,16 @@
-class Solution:
-    def divisorSubstrings(self, num: int, k: int) -> int:
-        
-        pow = 10**k
-        n =  num
-        count =  0
-        
-        while n:
-            
-            d = n%pow
-            print(d,n,pow)
-            if d and num%d==0:
-                
-                count += 1
-            
-            if n//pow==0:
-                break
-            
-            n //= 10
-            
-        
-        return count
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        s = str(num)
-        
-        front = 0
-        
-        curr = 0
-        
-        count = 0
-        
-        for rear in range(len(s)):
-            
-            curr = curr*10 + (ord(s[rear])-48)
-            
-            if rear - front + 1 == k:
-                
-                if curr and num % curr == 0:
-                    
-                    count +=1
-                    
-                curr = curr - (ord(s[front])-48) * 10**(k-1)
-                front +=1
-            
-        return count
+class Solution {
+public:
+    int divisorSubstrings(int num, int k) {
+        int res = 0, cur = 0, pow = 1;
+        for (int n = num; n > 0; n /= 10) {
+            cur += (n % 10) * pow;
+            if (--k > 0)
+                pow *= 10;
+            else {
+                res += cur && !(num % cur);
+                cur /= 10;
+            }
+        }
+        return res;
+    }
+};
