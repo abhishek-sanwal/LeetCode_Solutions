1e9ed1fdc31b5fd7012d9c5ca532ba13b00f35ae
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2023-05-11 21:42:45 +0530

    Time: 205 ms (47.77%), Space: 16.6 MB (36.48%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.py
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -16,9 +16,29 @@ class Solution:
                     else:
                         
                         dp[i][j] = max(dp[i-1][j],dp[i][j-1])
-                        
-                #print(dp)
             
             return dp[-1][-1]
         
+        def lcs1(s1,s2):
+            
+            dp = [0]*(len(s2)+1)
+            
+            new_dp =[0]*(len(s2)+1)
+            
+            for i in range(1,len(s1)+1):
+                
+                for j in range(1,len(s2)+1):
+                    
+                    if s1[i-1] == s2[j-1]:
+                        
+                        new_dp[j] = dp[j-1] + 1
+                        
+                    else:
+                        
+                        new_dp[j] = max(dp[j],new_dp[j-1])
+                                        
+                dp = new_dp[:]
+                
+            return new_dp[-1]
+                                        
         return lcs(nums1,nums2)
