7ff3077f4551e2c08f1f613497fcba6d87a8de71
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2023-06-01 12:03:38 +0530

    Time: 51 ms (20.76%), Space: 16.5 MB (10.26%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,42 @@
+from math import comb
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        row,col = m,n
+        
+        dp = [[0 for i in range(col)] for j in range(row)]
+        
+        for i in range(row):
+            
+            dp[i][0] = 1
+        
+        for j in range(col):
+            
+            dp[0][j] = 1
+        
+        for i in range(1,row):
+            
+            for j in range(1,col):
+                
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+            
+        return dp[-1][-1]
+                
+                
+        
+        def isValid(i,j):
+
+            return i>-1 and i<n and j>-1 and j<m
+        
+        def recursion(i,j):
+            
+            if isValid(i,j):
+                
+                if i==n-1 and j==m-1:
+                    return 1
+                
+                return recursion(i+1,j) + recursion(i,j+1)
+            
+            return 0
+        
+        return recursion(0,0)
