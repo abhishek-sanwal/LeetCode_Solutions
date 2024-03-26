1bd2a2b3ad55b88aba59ce333b1deed74c37965e
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-03-26 18:52:39 +0530

    Time: 1100 ms (49.60%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,8 +1,8 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 
 
 select w1.id
 from weather w1
 ,weather w2
-where datediff(w1.recordDate,w2.recordDate) = 1
+where w1.recordDate - w2.recordDate = 1
 and w1.temperature > w2.temperature;
