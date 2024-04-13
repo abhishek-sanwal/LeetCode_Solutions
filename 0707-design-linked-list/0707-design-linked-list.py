a4e2366444223f1f844f59779ef8305293071b03
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-04-13 23:50:13 +0530

    Time: 94 ms (95.86%), Space: 12.3 MB (76.82%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.py
@@ -0,0 +1,82 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        return str(self.val) + "->" + str(self.next)
+
+
+class MyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def get(self, index):
+        if index < 0 or index >= self.size:
+            return -1
+        
+        current = self.head
+        for _ in range(index):
+            current = current.next
+        return current.val
+
+    def addAtHead(self, val):
+        node = ListNode(val)
+        node.next = self.head
+        self.head = node
+        
+        if self.size == 0:
+            self.tail = node
+        
+        self.size += 1
+
+    def addAtTail(self, val):
+        node = ListNode(val)
+        if self.size == 0:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        
+        self.size += 1
+
+    def addAtIndex(self, index, val):
+        if index < 0 or index > self.size:
+            return
+        
+        if index == 0:
+            self.addAtHead(val)
+        elif index == self.size:
+            self.addAtTail(val)
+        else:
+            current = self.head
+            for _ in range(index - 1):
+                current = current.next
+            
+            node = ListNode(val)
+            node.next = current.next
+            current.next = node
+            
+            self.size += 1
+
+    def deleteAtIndex(self, index):
+        if index < 0 or index >= self.size:
+            return
+        
+        if index == 0:
+            self.head = self.head.next
+            if self.size == 1:
+                self.tail = None
+        else:
+            prev = self.head
+            for _ in range(index - 1):
+                prev = prev.next
+            
+            prev.next = prev.next.next
+            if index == self.size - 1:
+                self.tail = prev
+        
+        self.size -= 1
