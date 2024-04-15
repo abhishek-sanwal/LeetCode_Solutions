60731af40ee4ae889a1d8061adca1e4771381333
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-04-15 17:46:57 +0530

    Time: 596 ms (30.22%), Space: 77.9 MB (46.08%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,47 +1,130 @@
-class Node:
-    def __init__(self, k, v):
-        self.key = k
-        self.val = v
-        self.prev = None
-        self.next = None
 
-class LRUCache:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.dic = dict()
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
+class ListNode:
+    
+    def __init__(self,val, key , prev = None, nextt = None):
+        
+        self.val = val
+        self.prev = prev
+        self.next = nextt
+        self.key = key
+        
+class DoublyLinkedList:
+    
+    def __init__(self):
+        
+        self.head = ListNode(-1,-1)
+        self.tail = ListNode(-1,-1)
         self.head.next = self.tail
         self.tail.prev = self.head
+        
+    # 0(1)
+    def delete_last_element(self):
+        
+        node = self.tail.prev
+        
+        node.prev.next = self.tail
+        self.tail.prev = node.prev
+        
+        return node
+        
+    
+    #0(1)
+    def insertAfterHead(self,val,key):
+        
+        newNode = ListNode(val,key)
+        temp = self.head
+        x = temp.next
+        temp.next = newNode
+        newNode.prev = temp
+        
+        if self.head.next == self.tail:
+            #print("true","empty")
+            
+            newNode.next = self.tail
+            self.tail.prev = newNode
+            return newNode
+        
+        newNode.next = x
+        x.prev = newNode
+        
+        return newNode
+    
+    # 0(1) time
+    def deleteNode(self,node):
+        
+        x = node.prev
+        y = node.next
+        x.next = y
+        y.prev = x
+        
+        return
+            
+class LRUCache(object):
+    
+    
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        
+        self.mapp = dict()
+        self.dl = DoublyLinkedList()
+        self.size = capacity
 
     def get(self, key):
-        if key in self.dic:
-            n = self.dic[key]
-            self._remove(n)
-            self._add(n)
-            return n.val
-        return -1
+        """
+        :type key: int
+        :rtype: int
+        """
+        #self.dl.printList()
+        
+        if key not in self.mapp:
+            return -1
+        
+        node = self.mapp[key]
+        
+        x = node.val
+        
+        # Delete Node
+        #0(1)
+        self.dl.deleteNode(node)
+        
+        # Insert node with same value
+        # 0(1)
+        self.mapp[key] = self.dl.insertAfterHead(x,key)
+        return x
+        
 
     def put(self, key, value):
-        if key in self.dic:
-            self._remove(self.dic[key])
-        n = Node(key, value)
-        self._add(n)
-        self.dic[key] = n
-        if len(self.dic) > self.capacity:
-            n = self.head.next
-            self._remove(n)
-            del self.dic[n.key]
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        if key in self.mapp:
 
-    def _remove(self, node):
-        p = node.prev
-        n = node.next
-        p.next = n
-        n.prev = p
+            # Delete Node
+            self.dl.deleteNode(self.mapp[key])
+            #0(1)
+        
+        elif len(self.mapp) == self.size:
+            #0(1)
+            x = self.dl.delete_last_element()
+            del self.mapp[x.key]
+            del x
+            
+        #0(1)
+        self.mapp[key] =  self.dl.insertAfterHead(value,key)
+        #print(self.dl.printList())
+        #print(self.mapp)
+        return None
+        
+            
+        
+        
 
-    def _add(self, node):
-        p = self.tail.prev
-        p.next = node
-        self.tail.prev = node
-        node.prev = p
-        node.next = self.tail
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
