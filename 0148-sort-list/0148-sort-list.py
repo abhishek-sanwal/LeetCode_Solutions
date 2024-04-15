4e7ca1eeeb2208aa99b41093065e39f4c6d64c8b
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-04-16 01:03:37 +0530

    Time: 932 ms (5.01%), Space: 37.4 MB (16.38%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -6,6 +6,67 @@
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
+        # Merge sort  on Linked List
+        
+        def merge(head1,head2):
+            
+            dh = dummyHead = ListNode(-1)
+            
+            while head1 or head2:
+                
+                if head1 and head2:
+                    
+                    if head1.val < head2.val:
+                        node = ListNode(head1.val)
+                        dh.next = node
+                        dh = node
+                        head1 = head1.next
+                        
+                    else:
+                        node = ListNode(head2.val)
+                        dh.next = node
+                        dh = node
+                        head2 = head2.next
+                    
+                elif head1:
+                    
+                    node = ListNode(head1.val)
+                    dh.next = node
+                    dh = node
+                    head1 = head1.next
+                    
+                elif head2:
+                    node = ListNode(head2.val)
+                    dh.next = node
+                    dh = node
+                    head2 = head2.next
+                
+            return dummyHead.next
+        
+        def merge_sort(head):
+            
+            if not head or not head.next:
+                
+                return head
+            
+            # Find middle element of linked list and cut it in two parts
+            
+            slow = fast = head
+            prev = None
+            
+            while fast and fast.next:
+                
+                fast = fast.next.next
+                prev = slow
+                slow = slow.next
+            
+            prev.next = None
+            
+            left = merge_sort(head)
+            right = merge_sort(slow)
+            
+            return merge(left,right)
+        return merge_sort(head)
         arr = []
         temp = prev = head
         while head:
