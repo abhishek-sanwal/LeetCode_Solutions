da8df3e5333e436aaab4ad28945a0a3a979a89be
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-04-17 22:50:46 +0530

    Time: 440 ms (27.13%), Space: 18.6 MB (47.93%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.py
+++ 0147-insertion-sort-list/0147-insertion-sort-list.py
@@ -5,40 +5,19 @@
 #         self.next = next
 class Solution:
     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        dummy = ListNode(-float('inf'))
-        while head:
-            nxt = head.next
-            cur = dummy
-            while cur.next and cur.next.val < head.val: cur = cur.next
-            head.next = cur.next
-            cur.next = head
-            head = nxt
-        return dummy.next
-        
-        if not(head and head.next):
+        dummy = ListNode(-1)
+        temp = head
+        while temp:
+            nxt = temp.next
+            curr = dummy
             
-            return head
-        
-        dummyHead = dh = ListNode(-1,head)
-        curr =  head.next
-        # dh ->[4,2,1,3]
-            #temp =4  prev =dh curr = 2
-            #x = 1
-        while curr:
-            temp = head
-            prev = dh
-            while temp.val < curr.val:
-                prev = temp
-                temp = temp.next
-            
-            # Insert after prev and before temp
-            prev.next = curr
-            x = curr.next
-            
-            if curr != temp:
-                curr.next = temp
+            while curr.next and curr.next.val < temp.val:
                 
-            curr = x
-            print(dh.next)
-        return dummyHead.next
+                curr = curr.next
+                
+            # Insert temp between curr and curr.next   
+            temp.next = curr.next
+            curr.next = temp
+            temp = nxt
+            
+        return dummy.next
