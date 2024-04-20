9c12c472818213edcc99dc95be905ea2d22773d7
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-04-20 23:38:58 +0530

    Time: 91 ms (47.52%), Space: 22.1 MB (6.12%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,12 +5,31 @@ class Solution:
         
         mapp = {}
         
+        #0(N)
         for i in nums:
             
             mapp[i] = mapp.get(i,0) + 1
-            
-        min_heap = []
         
+        bucket = [[] for i in range(len(nums)+1)]
+        
+        # Bucket sort on frequencies
+        for i in mapp:
+            
+            bucket[mapp[i]].append(i)
+            
+        ans = list() 
+        
+        for i in range(len(bucket)-1,-1,-1):
+            
+            if bucket[i]:
+                ans.extend(bucket[i])
+            if len(ans) ==k:
+                break
+        return ans
+            
+        
+        min_heap = []
+        # 0(nlogk)
         for i in mapp:
             
             heappush(min_heap,[mapp[i],i])
@@ -18,7 +37,7 @@ class Solution:
             if len(min_heap) > k:
 
                 heappop(min_heap)
-                
+        #0(n)
         return [i[1] for i in min_heap]
             
         
