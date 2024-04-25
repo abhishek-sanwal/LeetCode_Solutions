0e5e2a538799a0a7442471c899f06f6d89e33c0e
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-04-26 00:34:09 +0530

    Time: 58 ms (95.54%), Space: 18.7 MB (32.15%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        
+        stack = list()
+        s = [i for i in s]
+        match = {
+            "(" :")"
+        }
+        
+        for i,ele in enumerate(s):
+            
+            if ele == ")":
+                
+                if not stack:
+                    s[i] = ""
+                    continue
+                stack.pop()
+                
+            elif ele == "(":
+                stack.append(i)
+        
+        while stack:
+            s[stack.pop()] = ""
+        
+        return "".join(s)
