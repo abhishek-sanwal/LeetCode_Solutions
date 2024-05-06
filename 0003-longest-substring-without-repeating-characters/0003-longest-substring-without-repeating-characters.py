38f7564c3bee06effb3988cea96cb456480ad7c7
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-05-06 21:59:33 +0530

    Time: 55 ms (59.60%), Space: 16.6 MB (60.45%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,45 +1,15 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        mapp = {}
-        
-        # Sliding window problem
-        
-        mapp = {}
-        
-        ptr = maxi = 0
-        
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        mapp = dict()
+        front = maxi = 0
         for i,ele in enumerate(s):
             
             if ele in mapp:
-                ptr = max(ptr,mapp[ele] + 1)
                 
+                front = max(front,mapp[ele]+1)
+            
+            maxi = max(maxi,i-front+1)
+            
             mapp[ele] = i
-            maxi = max(maxi,i-ptr + 1)
-        
-        return maxi
-        
-        maxi = front = 0
-        
-        for rear in range(len(s)):
-            
-            if s[rear] in mapp:
-                
-                idx = mapp[s[rear]]
-                
-                for j in range(front,idx+1):
-                    
-                    del mapp[s[j]]
-                    
-                front = idx + 1
-            
-            mapp[s[rear]] = rear
-            
-            maxi = max(maxi,len(mapp))
             
         return maxi
-            
-            
