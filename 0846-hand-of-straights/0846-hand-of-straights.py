dc1423f175ab0b19c9112ce605c32479ff36f508
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-06 14:15:48 +0530

    Time: 161 ms (64.60%), Space: 18.9 MB (7.28%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,6 +1,91 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         
+        if len(hand) % groupSize != 0:
+            return False
+
+        # Counter to store the count of each card value
+        card_count = Counter(hand)
+
+        for card in hand:
+            start_card = card
+            # Find the start of the potential straight sequence
+            while card_count[start_card - 1]:
+                start_card -= 1
+
+            # Process the sequence starting from start_card
+            while start_card <= card:
+                # Check if we can form a consecutive sequence
+                # of groupSize cards
+                while card_count[start_card]:
+                    for next_card in range(start_card, start_card + groupSize):
+                        if not card_count[next_card]:
+                            return False
+                        card_count[next_card] -= 1
+
+                start_card += 1 
+
+        return True
+        
+        mapp = Counter(hand)
+        
+        groupsize = 3
+        
+#         [5,5,6,7,8]
+        
+#         [5,5,6,6,7,7,8,8]
+#         5:1  0 [5,6,7]  [6,7,8]
+#         6:2  1
+#         7:2  1
+#         8:1  1
+#         [1,2,3,6,2,3,4,7,8]
+
+        # start_element = 5 element = 8
+        for element in hand:
+            
+            start_element = element
+            
+            while mapp[start_element-1] > 0:
+                
+                start_element -= 1
+            
+            # Can not from streak less than groupSizes
+            # So that start_element should be less than this.
+            while start_element <= element - groupSize + 1:
+                
+                for curr_element in range(start_element,start_element + groupSize):
+                    
+                    # Streaks can not formed
+                    if mapp[curr_element] == 0:
+                        
+                        return False
+                    
+                    mapp[curr_elemnt] -= 1
+                    
+                # No streaks can not be formed with this start_element
+                if mapp[start_element] == 0:
+                    start += 1
+                
+        return True
+                
+                
+                
+        return any(mapp[i] for i in mapp)
+                
+                
+            
+            
+            
+            
+            
+            
+        
+        
+        
+        
+        
+        # Sorting and Binary search
+        
         if not hand or len(hand) %  groupSize:
             
             return False
