ee9c0d49005198d800720e657a30264d42fd0905
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-06 13:22:33 +0530

    Time: 170 ms (39.00%), Space: 18.4 MB (50.76%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -13,6 +13,40 @@ class Solution:
         
         for i in range(1,len(hand)):
             
+            curr_element = hand[i]
+            
+            # find leftmost element less than curr_element by bs
+            
+            pos = bisect_left(previous,[curr_element,1])
+            
+            # Create operation
+            if pos == 0 or previous[pos-1][0] != curr_element - 1 or previous[pos-1][1] ==groupSize:
+                
+                previous.append([curr_element,1])
+                
+            # Repalce operation
+            else:
+                previous[pos-1] = [curr_element, previous[pos-1][1] + 1]
+                
+            # print(previous,pos-1)
+            if len(previous) > len(hand) // groupSize:
+                
+                return False
+            
+        return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # Without binary search         
+        for i in range(1,len(hand)):
+            
             match = False
             
             for index in range(len(previous)):
