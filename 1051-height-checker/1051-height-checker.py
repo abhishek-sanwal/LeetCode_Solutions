e845e107162f963d71976b1f70ceac19ae85077c
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-10 11:24:25 +0530

    Time: 32 ms (91.92%), Space: 16.4 MB (77.64%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -1,14 +1,40 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         
+        def counting_sort(heights):
+            
+            bucket = [0]*(101)
+            
+            for ele in heights:
+                
+                bucket[ele] += 1
+                
+            return bucket
+        
         count = 0
         
-        arr = sorted(heights)
+        # arr = sorted(heights)
         
-        for i,j in zip(arr,heights):
+#         for i,j in zip(arr,heights):
             
-            if i != j:
+#             if i != j:
                 
+#                 count += 1
+                
+#         return count
+
+        bucket = counting_sort(heights)
+        i = 1
+        
+        for element in heights:
+            
+            while bucket[i] == 0:
+                
+                i += 1
+                
+            if i != element:
                 count += 1
+            bucket[i] -= 1
                 
         return count
+            
