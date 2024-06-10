99479a5cdf20193540f5e308e28074be69fd5f84
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-10 22:48:19 +0530

    Time: 42 ms (22.11%), Space: 16.5 MB (73.54%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -1,7 +1,59 @@
 class Solution:
     def numSteps(self, s: str) -> int:
         
+        
+        # 1101
+        
+#         if last bit is 1 add 2 to operations 
+        
+#         1+1 = 10 we will add 1 to that acc to operation
+#         and remove last bit
+        
+#         1 + 1 = 1 2 operation + one bit
+        
+        
+        carry = operations = 0
+        
+        for i in range(len(s)-1,0,-1):
+            
+            if int(s[i]) + carry == 1:
+                
+                operations += 1
+                
+                carry = 1
+            
+            operations += 1
+            
+        return operations + carry
+                
+            
+                
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         num = 0
+        # 0(N)
         for i in range(len(s)-1,-1,-1):
             
             if s[i] == "1":
@@ -9,7 +61,7 @@ class Solution:
                 # left shift
                 num += (1<< (len(s)-i-1))
             
-        print(num)
+        # 0(Log num)
         opr = 0
         while num != 1:
             
