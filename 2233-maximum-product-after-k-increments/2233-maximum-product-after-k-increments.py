98e1f4bdd17ddd41c7080b80bbeb727cf6b7b955
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-14 14:26:19 +0530

    Time: 977 ms (74.72%), Space: 27.2 MB (51.10%) - LeetHub

--- 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.py
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.py
@@ -1,24 +1,24 @@
 class Solution:
     def maximumProduct(self, nums: List[int], k: int) -> int:
         
-        counts = Counter(nums)
-        num = min(counts.keys())  # start from minimum number
+#         counts = Counter(nums)
+#         num = min(counts.keys())  # start from minimum number
         
-        while k > 0:
-            counts[num] -= 1 
-            counts[num+1] += 1
-            if counts[num] == 0:  # if there are no more elements in this "bucket" or number, then we go to the next highest num
-                num += 1
+#         while k > 0:
+#             counts[num] -= 1 
+#             counts[num+1] += 1
+#             if counts[num] == 0:  # if there are no more elements in this "bucket" or number, then we go to the next highest num
+#                 num += 1
                 
-            k -= 1
+#             k -= 1
         
-        product = 1
-        mod = pow(10,9) + 7
-        for num, count in counts.items():
-            if count > 0:
-                product = (product * (num**count)) % mod
+#         product = 1
+#         mod = pow(10,9) + 7
+#         for num, count in counts.items():
+#             if count > 0:
+#                 product = (product * (num**count)) % mod
             
-        return product 
+#         return product 
         
         
         heapify(nums)
@@ -26,6 +26,10 @@ class Solution:
         for i in range(k):
             
             heappush(nums, heappop(nums) + 1)
+            
+        mod = pow(10,9) + 7
+        
+        return reduce(lambda x,y:(x*y) % mod, nums)
         
         sol = 1
         mod = 10**9 + 7
