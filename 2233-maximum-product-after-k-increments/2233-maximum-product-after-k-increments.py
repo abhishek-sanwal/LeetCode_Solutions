fb1a91162531f6c1d05008d73a84a5711045a159
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-14 14:40:20 +0530

    Time: 1147 ms (11.53%), Space: 33 MB (5.49%) - LeetHub

--- 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.py
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.py
@@ -1,25 +1,28 @@
 class Solution:
     def maximumProduct(self, nums: List[int], k: int) -> int:
         
-#         counts = Counter(nums)
-#         num = min(counts.keys())  # start from minimum number
+        counts = Counter(nums)
+        num = min(counts.keys())  # start from minimum number
         
-#         while k > 0:
-#             counts[num] -= 1 
-#             counts[num+1] += 1
-#             if counts[num] == 0:  # if there are no more elements in this "bucket" or number, then we go to the next highest num
-#                 num += 1
+        while k > 0:
+            counts[num] -= 1 
+            counts[num+1] += 1
+            if counts[num] == 0:  # if there are no more elements in this "bucket" or number, then we go to the next highest num
+                num += 1
                 
-#             k -= 1
+            k -= 1
         
-#         product = 1
-#         mod = pow(10,9) + 7
-#         for num, count in counts.items():
-#             if count > 0:
-#                 product = (product * (num**count)) % mod
+        product = 1
+        mod = pow(10,9) + 7
+        for num, count in counts.items():
+            if count > 0:
+                product = (product * pow(num,count,mod)) % mod
             
-#         return product 
+        return product 
         
+        # [2,3,3,6] k = 2
+        
+        # []
         
         heapify(nums)
         
