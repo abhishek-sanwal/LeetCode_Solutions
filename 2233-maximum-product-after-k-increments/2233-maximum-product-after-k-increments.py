c94dea6c5eb6bb9487001af18ac69932dfc37614
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-14 14:18:38 +0530

    Time: 4259 ms (5.49%), Space: 33.1 MB (5.49%) - LeetHub

--- 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.py
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.py
@@ -1,6 +1,25 @@
 class Solution:
     def maximumProduct(self, nums: List[int], k: int) -> int:
         
+        counts = Counter(nums)
+        num = min(counts.keys())  # start from minimum number
+        
+        while k > 0:
+            counts[num] -= 1 
+            counts[num+1] += 1
+            if counts[num] == 0:  # if there are no more elements in this "bucket" or number, then we go to the next highest num
+                num += 1
+                
+            k -= 1
+        
+        product = 1
+        for num, count in counts.items():
+            if count > 0:
+                product *= (num**count)
+            
+        return product % (10**9 + 7)
+        
+        
         heapify(nums)
         
         for i in range(k):
