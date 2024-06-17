a2f40c32607db748d525109fada2b339887540f2
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-17 22:05:31 +0530

    Time: 55 ms (98.83%), Space: 16.6 MB (39.72%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,25 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        
+        # The prime decompisition of c should not contain 
+        # prime decomposition does not contain any p^k
+        # where prime p = 3 mod 4 and k is odd
+        
+        n = c
+        
+        for i in range(2,int(n**0.5)+1):
+            
+            if n%i==0:
+                p = i
+                k = 0
+                while not n%i:
+                    k += 1
+                    n //= i
+                
+                if k%2 and p%4==3:
+                    
+                    return False
+        
+        return n%4!=3
+        
+        
