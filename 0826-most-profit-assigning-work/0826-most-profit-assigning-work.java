f668035bad526caf6d8d9c3008da1ade89a52460
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-18 14:13:45 +0530

    Time: 10 ms (94.96%), Space: 46.2 MB (13.27%) - LeetHub

---
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxProfitAssignment(
+        int[] difficulty,
+        int[] profit,
+        int[] worker
+    ) {
+        
+        int maximumAbility = Arrays.stream(worker).max().getAsInt();
+        // System.out.println(maximumAbility);
+        int[] ability = new int[maximumAbility+1];
+        
+        for(int i=0;i<difficulty.length;++i){
+            
+            if(difficulty[i] <= maximumAbility )
+                    ability[difficulty[i]] = Math.max(ability[difficulty[i]], profit[i]);
+            
+        }
+        
+        // Create prefix maxima array
+        for(int i=1;i<=maximumAbility;++i)
+                ability[i] = Math.max(ability[i], ability[i-1]);
+        
+        int totalProfit = 0;
+        for(int currWorker:worker){
+            
+            totalProfit += ability[currWorker];
+            
+        }
+        return totalProfit;
+        
+    }
+}
