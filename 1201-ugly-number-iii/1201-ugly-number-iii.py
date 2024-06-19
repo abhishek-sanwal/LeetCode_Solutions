89d29f63c9af994ac32d885bf09970b9e69be55f
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-19 23:46:03 +0530

    Time: 36 ms (56.96%), Space: 16.4 MB (76.38%) - LeetHub

--- 1201-ugly-number-iii/1201-ugly-number-iii.py
+++ 1201-ugly-number-iii/1201-ugly-number-iii.py
@@ -6,36 +6,15 @@ class Solution:
         ca = c*a//gcd(c, a)
         abc = ab*c//gcd(ab, c)
         
-        lo, hi = 1, n*min(a, b, c)
-        while lo <= hi: 
-            mid = lo + hi >> 1
-            if mid//a + mid//b + mid//c - mid//ab - mid//bc - mid//ca + mid//abc < n:
-                lo = mid + 1
-            else:
-                hi = mid - 1
-                
-        return lo 
-        
         low = 0
         
-        high = 10**18
-        
-        factos = [a,b,c]
-        
-        def lcm(a,b):
-            
-            return (a*b) // gcd(a,b)
-        
-        def lcm(*lis):
-            
-            return reduce(lcm,lis)
-            
+        high = n*min(a,b,c)
         
         while low <= high:
             
             mid = (low + high)//2
             
-            count = mid//a + mid//b + mid//c - mid//lcm(a,b) - mid//lcm(a,c) - mid//lcm(b,c) + mid//lcm(a,b,c)
+            count = mid//a + mid//b + mid//c - mid//ab- mid//bc- mid//ca + mid//abc
             
             if count >= n:
                 
