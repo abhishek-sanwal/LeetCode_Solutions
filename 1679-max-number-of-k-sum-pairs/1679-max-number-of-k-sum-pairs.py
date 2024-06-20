a60f34fb4323aea66163138978791ec166bf85b1
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-20 22:53:28 +0530

    Time: 480 ms (87.11%), Space: 28.7 MB (65.57%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        
+        count = 0
+        # [1,3,3,3,4]
+        low = 0
+        high = len(nums)-1
+#         [4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4]
+#         [1,1,1,1,2,2,2,2,3,3,3,4,4,4,5,5,5,5]
+        
+        nums.sort()
+        while low < high:
+            # print(nums[low],nums[high])
+            if nums[low] + nums[high] == k:
+
+                low += 1
+                high -= 1
+                count += 1
+            
+            elif nums[low] + nums[high] > k:
+
+                high -= 1
+
+            else:
+                low += 1
+
+        return count
