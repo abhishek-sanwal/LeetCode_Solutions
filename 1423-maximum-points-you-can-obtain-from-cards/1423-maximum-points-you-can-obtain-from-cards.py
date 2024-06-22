d35bf0fda0c5d037e0619fc1d9085dd6ae2c849b
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-22 23:59:28 +0530

    Time: 351 ms (9.75%), Space: 29.6 MB (90.19%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -1,7 +1,27 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
         
-        ans = 0
+        # ans = 0
+        # Minimum size subarray sum with length n-k
+        n = len(cardPoints)
+        nums = cardPoints
+        front = sumi = sum =  0
+        ans = 10**18
+        for rear in range(len(cardPoints)):
+            
+            sumi += cardPoints[rear]
+            sum += cardPoints[rear]
+            
+            if rear-front+1 > n-k:
+                sumi -= nums[front]
+                front += 1
+                
+            if rear-front +1 == n-k:
+                
+                ans = min(ans,sumi)
+        
+        return sum-ans
+            
         
         front = sumi = pref =  0
         prefix = [0]*len(cardPoints)
