97aa95b26290c252abadac8ad93918d9a33bb67e
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-22 19:07:26 +0530

    Time: 885 ms (63.89%), Space: 30.7 MB (92.57%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -1,21 +1,28 @@
-class Solution(object):
-    def shortestSubarray(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        A = nums
-        K = k
-        N = len(A)
-        B = [0] * (N + 1)
-        for i in range(N): B[i + 1] = B[i] + A[i]
-        d = collections.deque()
-        res = N + 1
-        for i in xrange(N + 1):
-            while d and B[i] - B[d[0]] >= K: res = min(res, i - d.popleft())
-            while d and B[i] <= B[d[-1]]: d.pop()
-            d.append(i)
-        return res if res <= N else -1
-
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
         
+#         prefix[j] -prefix[i] >= k and j-i is minimized.
+        
+#         prefix[j] >= prefix[i]
+#         prefix[j] - k >= prefix[i]
+#         pass
+    
+#         x1 < x2 and p[x2] <= p[x1]
+
+        n = len(nums)
+        prefix = [0] * (n + 1)
+        for i in range(n): 
+            prefix[i + 1] = prefix[i] + nums[i]
+            
+        d = collections.deque()
+        res = n + 1
+        for j in range(n + 1):
+            # prefix[d[0]] contains i so prefix[j]-prefix[i] >= k
+            while d and prefix[j] <= prefix[d[-1]]: 
+                d.pop()
+            while d and prefix[j] - prefix[d[0]] >= k: # d contains i's
+                res = min(res, j - d.popleft())
+            # print(d,"before")
+            d.append(j)
+            # print(d,"after")
+        return -1 if res==n+1  else res
