16779d860703d7eb6f3f8d382a2e1f2751e5b923
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-27 00:57:18 +0530

    Time: 299 ms (44.56%), Space: 37.7 MB (6.95%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.py
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.py
@@ -7,22 +7,20 @@ class Solution:
         temp = [[nums2[i],nums1[i]] for i in range(len(nums2))]
         
         temp.sort(key=lambda x:-x[0])
-        print(temp)
+        # print(temp)
         min_heap = list()
         
         sumi = ans =  0
-        
+        mod = (10**9) + 7
         for i in range(len(temp)):
             
             heappush(min_heap, temp[i][1])
             sumi += temp[i][1]
-            # print(min_heap,sumi)
             if len(min_heap) > k:
                 
                 sumi -=heappop(min_heap)
             
-            # print(sumi,min_heap)
-            ans = max(ans, sumi*temp[i][0])
+            ans = max(ans, (sumi*temp[i][0]))
             
-        return ans% (10**9 + 7)
+        return ans%mod
         
