89223813d2ff3fecb069c39d566d6abd43c5c953
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-29 16:51:11 +0530

    Time: 83 ms (87.91%), Space: 18.2 MB (88.71%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,40 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        # Kahn's algo for Topo sort
+        
+        indegree = [0]*numCourses
+        
+        adj = defaultdict(list)
+        
+        for u,v in prerequisites:
+            
+            adj[u].append(v)
+            indegree[v] += 1
+            
+        que = deque()
+        # Find nodes with zero degree or zero dependecies or starter nodes
+        order = list()
+        for node in range(numCourses):
+            
+            if not indegree[node]:
+                
+                que.append(node)
+                order.append(node)
+                
+        while que:
+            
+            for _ in range(len(que)):
+                
+                node = que.popleft()
+                
+                # Vertices which can be accessed from node
+                for adjacent in adj[node]:
+                    
+                    indegree[adjacent] -= 1
+                    
+                    if not indegree[adjacent]:
+                        que.append(adjacent)
+                        order.append(adjacent)
+                        
+        return len(order) == numCourses
