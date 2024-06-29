e58c4070cd603421e13f08032de98794f6272806
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-29 16:24:59 +0530

    Time: 440 ms (91.42%), Space: 44.5 MB (54.82%) - LeetHub

---
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -0,0 +1,41 @@
+class Solution:
+    def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        
+        anchestors = [set() for _ in range(n)]
+        
+        adj = defaultdict(list)
+        for u,v in edges:
+            
+            adj[u].append(v)
+        
+        stack = list()
+        vis = set()
+        def dfs(node):
+            
+            vis.add(node)
+            
+            for adjacent in adj[node]:
+                
+                if adjacent not in vis:
+                    dfs(adjacent)
+            
+            
+            stack.append(node)
+            
+        
+        for node in range(n):
+            
+            if node not in vis:
+                
+                dfs(node)
+        
+        while stack:
+            node = stack.pop()
+            for vertice in adj[node]:
+                anchestors[vertice].add(node)
+                anchestors[vertice].update(anchestors[node])
+        del stack     
+
+        for index in range(len(anchestors)):
+            anchestors[index] = sorted((anchestors[index]))
+        return anchestors
