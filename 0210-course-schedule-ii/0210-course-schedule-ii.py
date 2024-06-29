20d402cb2734494154400e27b7a5e6cecdbfa7e2
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-06-29 17:02:55 +0530

    Time: 89 ms (66.03%), Space: 18.3 MB (56.34%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,13 +1,12 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        
-        # Kahn's algo for Topo sort
+       # Kahn's algo for Topo sort
         
         indegree = [0]*numCourses
         
         adj = defaultdict(list)
         
-        for u,v in prerequisites:
+        for v,u in prerequisites:
             
             adj[u].append(v)
             indegree[v] += 1
@@ -15,74 +14,28 @@ class Solution:
         que = deque()
         # Find nodes with zero degree or zero dependecies or starter nodes
         order = list()
+        vis = set()
+        
         for node in range(numCourses):
             
             if not indegree[node]:
                 
                 que.append(node)
-                order.append(node)
                 
         while que:
             
             for _ in range(len(que)):
-                
                 node = que.popleft()
-                
+                order.append(node)
                 # Vertices which can be accessed from node
                 for adjacent in adj[node]:
-                    
-                    indegree[adjacent] -= 1
-                    
-                    if not indegree[adjacent]:
-                        que.append(adjacent)
-                        order.append(adjacent)
-                        
-        return reversed(order) if len(order) == numCourses else list()
-        
-        adj = defaultdict(list)
-        
-        for u,v in prerequisites:
-            
-            adj[u].append(v)
-            
-        
-        stack = list()
-        vis = set()
-        def dfs(node):
-            
-            vis.add(node)
-            
-            for adjacent in adj[node]:
-                
-                if adjacent not in vis:
-                    dfs(adjacent)
-        
-            stack.append(node)
-            
-        for node in range(numCourses):
-            
-            if node not in vis:
-                
-                dfs(node)
-        return stack
-        stack = list()
-        
-        def dfs(node):
 
-            vis.add(node)
-            
-            for adja in adj[node]:
-                
-                if adja not in vis:
-                    dfs(adja)
-                    
-            stack.append(node)
-            
-            return None
+                    if adjacent not in vis:
+                        indegree[adjacent] -= 1
+
+                        if not indegree[adjacent]:
+                            que.append(adjacent)
+
+                        
+        return order if len(order) == numCourses else list()
         
-        vis = set()
-        
-        for node in range(numCourses):
-            dfs(node)
-        print(stack)
-        return stack[::-1]
