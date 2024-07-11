d94040127d1a92ae25b40c01a4d47ea74d08626f
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-07-11 22:37:05 +0530

    Time: 104 ms (36.46%), Space: 13.8 MB (65.10%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,10 +1,9 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         
-        xor = nums[0]
         
-        for i in range(1,len(nums)):
-            
-            xor ^= nums[i]
-            
-        return xor
+        return reduce(lambda x,y:x^y, nums)
