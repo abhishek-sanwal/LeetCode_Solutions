28dee80b97f7a59a3e8d73a51dc0e05eed40ab5c
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-07-11 22:39:00 +0530

    Time: 41 ms (37.27%), Space: 16.5 MB (54.96%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        
+        
+        xor = 0
+        
+        for i in s:
+            xor ^= ord(i)
+            
+        for i in t:
+            
+            xor ^= ord(i)
+        
+        return chr(xor)
