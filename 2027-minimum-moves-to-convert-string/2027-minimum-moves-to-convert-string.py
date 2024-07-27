25230ab8071bbd929bfc27256c67cb3525efb71f
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-07-27 13:45:30 +0530

    Time: 42 ms (20.40%), Space: 16.4 MB (95.57%) - LeetHub

---
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumMoves(self, s: str) -> int:
+        
+        count =  i = 0
+        
+        while i < len(s):
+            
+            if s[i] == "X":
+                count += 1
+                
+                i += 3
+                
+            else:
+                i += 1
+        
+        return count
