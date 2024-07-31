6e1e4a0d3769dde758c6326883c40d8f62891daa
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-08-01 01:42:54 +0530

    Time: 46 ms (84.81%), Space: 17.3 MB (41.44%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.py
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.py
@@ -1,20 +1,30 @@
 class Solution:
     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
         
-        @lru_cache(maxsize=1000)
+        memo = {}
+        
         def recursion(curr_width,index, answer):
             
             if index == len(books):
                 
+                memo[(curr_width, index)] = answer
                 return answer
+            
+            if (curr_width, index) in memo:
+                
+                return memo[(curr_width, index)]
             
             if curr_width - books[index][0] >= 0 :
                 
-                return min(recursion(curr_width - books[index][0],index + 1, max(answer, books[index][1])), 
+                memo[(curr_width, index)] = min(recursion(curr_width - books[index][0],index + 1, max(answer, books[index][1])), 
                            answer + recursion(shelfWidth - books[index][0], index + 1, books[index][1]) ) 
+                
+                return memo[(curr_width, index)]
             
             
-            return answer + recursion(shelfWidth - books[index][0], index + 1, books[index][1])
+            memo[(curr_width, index)] = answer + recursion(shelfWidth - books[index][0], index + 1, books[index][1])
+            
+            return memo[(curr_width, index)]
         
         
         return recursion(shelfWidth, 0, 0)
