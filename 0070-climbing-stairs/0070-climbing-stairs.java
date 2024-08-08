ebcc2dc583873095ea57b2f83ee59465a2cb0e75
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-08-08 09:29:08 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (45.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,33 @@
 class Solution {
     public int climbStairs(int n) {
         
-        //  Creation of dp array        
-        int[] dp = new int[Math.max(3,n+1)];
+        if(n==1){
+            return 1;
+        }
         
-        // Initilization
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        // Bottom-up dp
-        for(int i=3;i<=n;++i)
-                dp[i] = dp[i-1] + dp[i-2];
-        return dp[n];
+        int prev = 1, curr = 2;
+        
+        for(int i=3;i<=n;i++){
+            
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+            
+        }
+        
+        return curr;
+        
+//         //  Creation of dp array        
+//         int[] dp = new int[Math.max(3,n+1)];
+        
+//         // Initilization
+//         dp[0] = 0;
+//         dp[1] = 1;
+//         dp[2] = 2;
+//         // Bottom-up dp
+//         for(int i=3;i<=n;++i)
+//                 dp[i] = dp[i-1] + dp[i-2];
+//         return dp[n];
         
         
     }
