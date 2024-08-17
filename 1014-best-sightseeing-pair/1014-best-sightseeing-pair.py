051243b4e2b4a48b2e19e2f20d6d92d59e42fa6f
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-08-17 23:54:00 +0530

    Time: 369 ms (33.23%), Space: 22.2 MB (49.35%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
@@ -2,6 +2,7 @@ class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
 
         right = [0]*len(values)
+        
         right[-1] = values[-1] - (len(values)-1)
 
         for i in range(len(values)-2,-1,-1):
@@ -10,8 +11,8 @@ class Solution:
 
         maxi = 0
 
-        for i in range(len(values)):
-            if i < len(values)-1:
-                maxi = max(maxi, values[i]+i + right[i+1])
+        for i in range(len(values)-1):
+            
+            maxi = max(maxi, values[i]+i + right[i+1])
 
         return maxi
