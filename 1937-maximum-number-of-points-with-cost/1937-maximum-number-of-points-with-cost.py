9e495bfe58b09b49586c39ce3bb3ecb8f8abe1ac
Abhishek Sanwal <90312068+abhishek-sanwal@users.noreply.github.com>
2024-08-17 23:39:35 +0530

    Time: 1546 ms (55.61%), Space: 50.9 MB (49.76%) - LeetHub

---
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.py
@@ -0,0 +1,44 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        
+        row = len(points)
+        col = len(points[0])
+
+        dp = list()
+        previous_dp = points[0]
+        
+        if row == 1:
+            return max(points[0])
+        # right i<j nums[i] + nums[j] - (j-i) = -j+i => nums[i] + i +  nums[j] - j
+        # left i>j nums[i] + nums[j] - (i-j) = nums[i] - i + nums[j] + j
+        # nums[i] + nums[j] - abs(i-j)
+        # nums[i] + nums[j] - i -j (i>j)
+        # nums[i] + nums[j] - j-i (j>i)
+        
+        for i in range(1,row):
+            
+            left = [previous_dp[0]]
+
+            for j in range(1, col):
+
+                left.append(max(left[-1]-1, previous_dp[j])) # i>j
+
+            right = [0]*col
+            right[-1] = previous_dp[-1]
+        
+            for j in range(col-2,-1,-1):
+
+                right[j] = max(right[j+1]-1, previous_dp[j]) # i<j
+            # print(left, right)
+            dp = []
+            for j in range(col):
+                maxi = -10**18
+                                  
+                maxi = max(maxi, points[i][j] + right[j], points[i][j] + left[j])
+                
+                dp.append(maxi)
+            # print(dp)
+            previous_dp = dp
+            dp = list()
+        
+        return max(previous_dp)
